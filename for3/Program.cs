5b3932fb29e6649f956a6a159b758321866c0a6d
Jengo2007 <zuyunovmagamed@gmail.com>
2024-12-04 16:57:02 +0300

    for6

---
+++ for3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+for (int i = b; i > a; i--)
+{
+    Console.WriteLine(i);
+}
